cd7ecc390980448f78312a980097a500d098c372
Lucas Ferreira <blooodcf@gmail.com>
2019-07-17 11:40:06 -0300

    Register user

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Nav from "./components/Nav";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Admin from "./pages/Admin";
+import Register from "./pages/Register";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import * as Routes from "./constants/routes";
 
@@ -15,6 +16,7 @@ function App() {
         <Switch>
           <Route path={Routes.HOME} component={Home} exact />
           <Route path={Routes.LOGIN} component={Login} />
+          <Route path={Routes.REGISTER} component={Register} />
           <Route path={Routes.ADMIN} component={Admin} />
         </Switch>
       </div>
